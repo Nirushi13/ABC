3b8817a43aee1b0d724050ab0e569bfefdaf7bd0
Nirushi13 <nirushikan2001@gmail.com>
2024-09-03 16:03:29 +0530

    Remove unused pages and update components

--- staff/src/components/Sidebar/Sidebar.jsx
+++ staff/src/components/Sidebar/Sidebar.jsx
@@ -7,16 +7,6 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-options">
-            <NavLink to="/add" className="sidebar-option">
-
-                <img src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
-            <NavLink  to="/list" className="sidebar-option">
-
-                <img src={assets.order_icon} alt="" />
-                <p>List Items</p>
-            </NavLink>
             <NavLink to="/orders" className="sidebar-option">
                 <img src={assets.order_icon} alt="" />
                 <p>Orders</p>
@@ -25,17 +15,9 @@ const Sidebar = () => {
                 <img src={assets.add_icon} alt="Queries" /> 
                 <p>Queries</p>
             </NavLink>
-            <NavLink to="/offers" className="sidebar-option">
-                <img src={assets.add_icon} alt="offers" /> 
-                <p>Offers</p>
-            </NavLink>
             <NavLink to="/reserve" className="sidebar-option">
                 <img src={assets.add_icon} alt="offers" /> 
                 <p>Reservations</p>
-            </NavLink>
-            <NavLink to="/gallery" className="sidebar-option">
-                <img src={assets.add_icon} alt="gallery" /> 
-                <p>Gallery</p>
             </NavLink>
         </div>
     </div>
